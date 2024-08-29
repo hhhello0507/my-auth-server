fc796e433af0947b3faa025484b15fff3c7062be
hhhello <hhhello0507@dgsw.hs.kr>
2024-08-29 21:24:40 +0900

    init

---
+++ Auth-iOS/Auth-iOS/Auth_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auth_iOSApp.swift
+//  Auth-iOS
+//
+//  Created by hhhello0507 on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Auth_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
